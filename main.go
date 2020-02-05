74d75840df0678420109de9126b5508c5d0cbd10
NAKAJIMA Takaaki <ryumei@users.noreply.github.com>
2020-02-06 06:23:13 +0900

    Accept default argument

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func greeting(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return fmt.Sprintf("Hello %s", name)
 }
 
--- main_test.go
+++ main_test.go
@@ -5,8 +5,15 @@ import (
 )
 
 func TestGreeting(t *testing.T) {
+	expected := "Hello Michael"
+	actual := greeting("Michael")
+	if actual != expected {
+		t.Errorf("got: %v\nwant: %v", actual, expected)
+	}
+}
+func TestGreetingWithoutArg(t *testing.T) {
 	expected := "Hello world"
-	actual := greeting("world")
+	actual := greeting("")
 	if actual != expected {
 		t.Errorf("got: %v\nwant: %v", actual, expected)
 	}
